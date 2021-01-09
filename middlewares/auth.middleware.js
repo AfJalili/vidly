3afcc527013959e4840fd7f1243e3b40ba8419ae
AfshinJalili <afshin6jalily@gmail.com>
2021-01-09 04:44:32 +0330

    add auth middleware and protect movies routes

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+import config from "../config.js";
+
+export default function auth(req, res, next) {
+    // check for existence
+    const token = req.header("x-auth-token");
+    if(!token) return res.status(401).send("Access Denied. No token provided!");
+
+    // validate token
+    try {
+        const decodedToken = jwt.verify(token, config.jwtSecret);
+        req.user = decodedToken;
+        next();
+    } catch (err) {
+            res.status(400).send("Invalid token!");
+    }
+
+}
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,16 +1,16 @@
 import express from "express";
 import {addMovie, deleteMovie, getAllMovies, getMovieById, updateMovie} from "../controllers/movies.controller.js";
-
+import auth from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 router.get("/", getAllMovies);
 
-router.post("/", addMovie);
+router.post("/", auth, addMovie);
 
 router.get("/:id", getMovieById);
 
-router.put("/:id", updateMovie);
+router.put("/:id", auth, updateMovie);
 
-router.delete("/:id", deleteMovie);
+router.delete("/:id", auth, deleteMovie);
 
 export default router;
